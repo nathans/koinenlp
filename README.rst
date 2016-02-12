70e2ef1fc0a32293eb6ea75690eca065372ba6ee
Nathan D. Smith <nathan@smithfam.info>
2016-02-12 06:37:37 -0800

    Prepare for pypi publication

---
+++ README.rst
@@ -0,0 +1,16 @@
+=========
+koine-nlp
+=========
+
+.. image:: https://travis-ci.org/nathans/koine-nlp.svg?branch=master
+    :target: https://travis-ci.org/nathans/koine-nlp
+
+.. image:: https://coveralls.io/repos/github/nathans/koine-nlp/badge.svg?branch=master :target: https://coveralls.io/github/nathans/koine-nlp?branch=master 
+
+
+A python3 library for common NLP-related tasks for Koine
+Greek.
+
+The fullest use of the library comes in the normalize() function,
+which combines all of the available methods to produce an end result
+which is suitable for indexing.
---
+++ setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='koine-nlp',
+      version='0.1',
+      description='',
+      long_description=open("README.rst").read(),
+      author='Nathan D. Smith',
+      author_email='nathan@smithfam.info',
+      # url='',
+      scripts=['koinenlp.py'],
+      license="MIT",
+      install_requires=[
+          "pypump==0.6",
+          "feedparser>=5.0",
+          "argparse>=1.0",
+          ],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Operating System :: OS Independent",
+                   "Operating System :: POSIX",
+                   "Intended Audience :: End Users/Desktop"])
